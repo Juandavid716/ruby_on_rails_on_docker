24024af748f77a514140efa1a8aec80ed4a6ca00
juandavid716 <jbojato@uninorte.edu.co>
2020-09-14 11:03:07 -0500

    initial login and sign up

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_10_164600) do
+ActiveRecord::Schema.define(version: 2020_09_14_154739) do
 
   create_table "tasks", force: :cascade do |t|
     t.string "title"
     t.text "description"
     t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end

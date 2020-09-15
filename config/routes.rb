beb1822061e5ef036c6ea899154187841927c478
juandavid716 <jbojato@uninorte.edu.co>
2020-09-15 12:09:43 -0500

    gems needed

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,14 @@
 Rails.application.routes.draw do
 
-  root 'dashboard#show'
+  resources :twitters, path: 'home'
+  root 'twitters#index'
 
-  #get 'sign_up', to: 'users/new'
-  get 'login', to: 'sessions#new'
-  get 'logout', to: 'sessions#destroy'
-  get 'init', to: 'tasks#init'
-  resources :sessions, only: [:create]
-  resources :users, only: [:new, :create]
+  # #get 'sign_up', to: 'users/new'
+  # get 'login', to: 'sessions#new'
+  # get 'logout', to: 'sessions#destroy'
+  # get 'init', to: 'tasks#init'
+  # resources :sessions, only: [:create]
+  # resources :users, only: [:new, :create]
   #resources :tasks
   
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_14_154739) do
+ActiveRecord::Schema.define(version: 2020_09_15_141658) do
 
   create_table "tasks", force: :cascade do |t|
     t.string "title"
     t.text "description"
     t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "twitters", force: :cascade do |t|
+    t.text "twitter"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end

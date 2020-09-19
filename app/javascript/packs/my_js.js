296529b8ecf0f2ad33b87ca95e16d6a3c26b6453
Jefferson Sierra <jeffersonjsierraa@live.com>
2020-09-19 15:26:32 -0500

    login page styles

--- app/javascript/packs/my_js.js
+++ app/javascript/packs/my_js.js
@@ -1,1 +1,19 @@
+document.addEventListener("DOMContentLoaded", function(event) {
+    const btn = document.getElementById('showModal');
+    const modalDlg = document.getElementById('image-modal');
+    const imageModalCloseBtn = document.getElementById('image-modal-close');
+    btn.addEventListener('click', function() {
+        modalDlg.classList.add('is-active');
+    });
+
+    imageModalCloseBtn.addEventListener('click', function() {
+        modalDlg.classList.remove('is-active');
+    });
+});
 console.log('Hello from My JS')
+
+/*$("#showmodal").click(function() {
+    $('.modal').addclass('is-active')
+        /*$(".modal").toggle("slow", function() {
+            // Animation complete.
+        });*/

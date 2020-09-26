f8abe2e7a6a08feb73870c7be8090c2de9e489db
juandavid716 <jbojato@uninorte.edu.co>
2020-09-26 00:45:53 -0500

    editing some views

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -11,6 +11,7 @@ Rails.application.config.assets.precompile += %w( index.css )
 Rails.application.config.assets.precompile += %w( signup.css )
 Rails.application.config.assets.precompile += %w( landingpage.css )
 Rails.application.config.assets.precompile += %w( feed.css )
+Rails.application.config.assets.precompile += %w( background.css )
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in the app/assets
 # folder are already added.

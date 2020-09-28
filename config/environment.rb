15050c685961aba65548ae5ff3ae412ca4171d08
juandavid716 <jbojato@uninorte.edu.co>
2020-09-27 22:55:23 -0500

    fixing environment v2

--- config/environment.rb
+++ config/environment.rb
@@ -4,8 +4,8 @@ require_relative 'application'
 # Initialize the Rails application.
 Rails.application.initialize!
 
-ActionMailer::Base.action_mailer.default_url_options = {:host => 'books-app-api.herokuapp.com', :protocol => 'http'} #I've also tried it without ":protocol => 'http'"
-ActionMailer::Base.action_mailer.delivery_method = :smtp
+config.action_mailer.default_url_options = {:host => 'books-app-api.herokuapp.com', :protocol => 'http'} #I've also tried it without ":protocol => 'http'"
+config.action_mailer.delivery_method = :smtp
 ActionMailer::Base.perform_deliveries = true
 ActionMailer::Base.smtp_settings = {
     address:'smtp.gmail.com',

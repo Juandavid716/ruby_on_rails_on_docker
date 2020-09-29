19106007b389128119fa6fdd6037f070fe9b7fae
juandavid716 <jbojato@uninorte.edu.co>
2020-09-29 13:00:33 -0500

    destroy added

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,5 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable, :confirmable
 
-  has_many :twitters
+  has_many :twitters, dependent: :destroy
 end

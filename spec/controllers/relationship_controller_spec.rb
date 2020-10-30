8c0f0ba197c2404441ea55779ed2a9ece056b52e
sierrajk <jeffersonjsierraa@live.com>
2020-10-29 22:13:55 -0500

    follow test

--- spec/controllers/relationship_controller_spec.rb
+++ spec/controllers/relationship_controller_spec.rb
@@ -2,4 +2,19 @@ require 'rails_helper'
 
 RSpec.describe RelationshipsController, "#create"  do
 
+  context "when a user use function follow" do
+    context "when user is logged in" do
+      let(:user) {create(:user)}
+      let(:user2) {create(:user)}
+      before do 
+        sign_in user
+        user.active_relationships.create(followed_id: user2.id)
+        #post :follow, params:         { user: user2}   
+      end
+
+      it "should have a follower list" do
+        expect(Relationship.all).not_to be_empty
+      end  
+    end
+  end
 end
